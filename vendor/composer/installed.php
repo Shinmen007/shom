bdf985e1adeeb850f6e74eb5a307f922cec7d6d3
Shinmen007 <shinmen07@hotmail.com>
2025-11-27 15:14:02 +0000

    Gin stole her from me sad me 2

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'shinmen/mvp',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'f0091aa734eb278e6749e1b4b15f069c849dba2f',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'shinmen/mvp' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'f0091aa734eb278e6749e1b4b15f069c849dba2f',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
